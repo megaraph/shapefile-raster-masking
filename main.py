1097d8fe43084a4c0f69e5926b0935a7eb58d346
Raphael Murillo <raphaelmurillo@Raphaels-MacBook-Pro.local>
2024-05-24 01:25:13 +0800

    minor changes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import fiona
 import rasterio
 import rasterio.mask
@@ -21,36 +22,40 @@ def main():
         rasters = get_raster_files(IMG_DIR_PATH)
         print(f"Located {len(rasters)} rasters...") # PROGRESS UPDATE
 
-        print("\n***MASKING RASTERS***...") # PROGRESS UPDATE
-        for index, raster in enumerate(rasters):
-            raster_path = str(raster)
+    # Open Raster files
+    print("\n***MASKING RASTERS***...") # PROGRESS UPDATE
+    for index, raster in enumerate(rasters):
+        raster_path = str(raster)
 
-            print(f"\nIn Progress: Masking Raster {index}...") # PROGRESS UPDATE
-            with rasterio.open(raster_path) as src:
-                out_image, out_transform = rasterio.mask.mask(src, shapes)
-                out_meta = src.meta
+        # Apply mask
+        print(f"\nIn Progress: Masking Raster {index}...") # PROGRESS UPDATE
+        with rasterio.open(raster_path) as src:
+            out_image, out_transform = rasterio.mask.mask(src, shapes)
+            out_meta = src.meta
 
-            out_filename = f"{raster.stem}{OUT_EXT}"
-            out_path = OUT_DIR_PATH.joinpath(out_filename)
+        out_filename = f"{raster.stem}{OUT_EXT}"
+        out_path = OUT_DIR_PATH.joinpath(out_filename)
 
-            # Create output directory if it doesn't exist
-            if not out_path.exists():
-                print("\nOutput directory does not exist...") # PROGRESS UPDATE
-                out_path.mkdir(parents=True, exist_ok=True)
-                print(f"\nOutput directory {out_path} successfully created...") # PROGRESS UPDATE
+        # Create output directory if it doesn't exist
+        if not out_path.exists():
+            print("\nOutput directory does not exist...") # PROGRESS UPDATE
+            out_path.mkdir(parents=True, exist_ok=True)
+            print(f"\nOutput directory {out_path} successfully created...") # PROGRESS UPDATE
 
-            print(f"\nIn Progress: Downloading Raster {out_path}...") # PROGRESS UPDATE
+        print(f"In Progress: Downloading Raster {out_path}...") # PROGRESS UPDATE
 
-            out_meta.update({"driver": "GTiff",
-                            "height": out_image.shape[1],
-                            "width": out_image.shape[2],
-                            "transform": out_transform,
-            })
+        out_meta.update({"driver": "GTiff",
+                        "height": out_image.shape[1],
+                        "width": out_image.shape[2],
+                        "transform": out_transform,
+        })
 
-            with rasterio.open(out_filename, "w", **out_meta) as dest:
-                dest.write(out_image)
+        with rasterio.open(out_filename, "w", **out_meta) as dest:
+            dest.write(out_image)
 
-            print(f"\nFinished: Successfully Masked Raster {index}!!") # PROGRESS UPDATE
+        print(f"Finished: Successfully Masked Raster {index}!!") # PROGRESS UPDATE
+
+    return 
 
 
 
@@ -60,4 +65,10 @@ def get_raster_files(dir_path):
     return files
 
 if __name__ == "__main__":
+    start_time = time.time()
     main()
+    end_time = time.time()
+
+    elapsed_time = end_time - start_time
+    print(f"Completed Masks in {elapsed_time} seconds")
+
